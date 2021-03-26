296cd1e6187d6c4bcfeaf2b8be5c2b159212b718
Nathan Wright <nqwright9311@gmail.com>
2021-03-26 22:56:10 +0000

    crack script update finished

--- crack.py
+++ crack.py
@@ -5,26 +5,29 @@ import crypt
 def main():
 
 	passwords = []
+
+# Open the parsed passwords file.
 	with open("justpasswords.txt", "r") as f:
 		passwords = f.readlines()
+# Iterating through the parsed passwords with user input choosing which password to use.
+# rstrip strips off the very end invsiible space.
+		encrypted_password = passwords[int(input("which line number?: "))].rstrip()
+	print ("encrypted password is" + encrypted_password)
 
-	print ("cracking password...")
-
+# Open the dictionary file of guesses.
 	dict_file = open("100000passwords.txt")
-	encrypted_password = passwords[1].rstrip()
+	for guess in dict_file.readlines():
 
-	print ("encrypted password looks to be... " + encrypted_password)
+		guess = guess.rstrip()
+		new_password = crypt.crypt(guess, encrypted_password)
 
-	for password in dict_file.readlines():
-		password = password.rstrip()
-		new_password = crypt.crypt(password, encrypted_password)
-
-		print ("Trying password " + password)
+		print ("Trying password " + guess)
 		print (encrypted_password + " % " + new_password)
 
+# if the passwords and guesses match do this
 		if (encrypted_password == new_password):
 			print("Password found!")
-			print("The cracked password is " + password)
+			print("The cracked password is " + guess)
 			exit()
 	print("NO password was found.")
 
